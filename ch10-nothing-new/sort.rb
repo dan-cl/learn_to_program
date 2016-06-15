b16fd4c6e3edee9b7c655c0b29642ec67c973872
Dan <djclissold@yahoo.co.uk>
2016-06-15 08:29:31 +0100

    sort program complete

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,34 @@
-def sort arr
-  # your code here
+def sort words
+  sorted = []
+  new_unsorted = []
+  unsorted = words.dup
+  	until unsorted.empty?
+  		smallest = unsorted.pop
+  		unsorted.each do |word|
+  			if word < smallest
+  				new_unsorted.push smallest
+  				smallest = word
+  			else
+  				new_unsorted.push word
+  			end
+  		end
+  			sorted.push smallest
+  			unsorted=new_unsorted.dup
+  			new_unsorted = []
+
+  end
+  	puts sorted
+    return sorted
+
 end
+
+
+words = []
+puts "Enter words."
+input=gets.chomp
+	while input != ""
+		words.push input
+		input = gets.chomp
+	end
+
+sort words
