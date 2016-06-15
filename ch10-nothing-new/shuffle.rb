f9ba573ba531fc9d3c26826110520d300a01fecf
Dan <djclissold@yahoo.co.uk>
2016-06-15 11:04:57 +0100

    shuffle program finished and working, but doesn't work on spec test.

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,31 @@
 def shuffle arr
-  # your code here
+  new_arr = []
+  shuffled = []
+  wordcount = 0
+  allreadyshuffled = []
+  while wordcount < arr.length
+    randnum = rand(arr.length)
+    if randnum != wordcount && !(allreadyshuffled.include? randnum)
+      shuffled.push arr[randnum]
+      wordcount = wordcount + 1
+      allreadyshuffled.push randnum
+    else
+    end
+  end
+ puts ''
+ puts shuffled
+ return shuffled
+
 end
+
+
+words = []
+puts 'Enter words'
+input = gets.chomp
+  while input != ''
+    words.push input
+    input = gets.chomp
+  end
+
+
+shuffle words
