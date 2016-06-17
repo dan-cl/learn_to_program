ac03f066f29ca7ac50f52d552a3d064fdf393fa7
Dan <djclissold@yahoo.co.uk>
2016-06-17 10:29:03 +0100

    Happy birthday program completed and working.

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,11 @@
-# your code here
+puts 'What year were you born in?'
+y = gets.chomp.to_i
+puts 'What month were you born?'
+m = gets.chomp.to_i
+puts 'What day of the month were you born?'
+d = gets.chomp.to_i
+birthday = Time.local(y, m, d)
+timenow = Time.new
+age_in_sec = timenow - birthday
+age_in_yrs = age_in_sec/60/60/24/365
+puts 'SPANK! ' * age_in_yrs
