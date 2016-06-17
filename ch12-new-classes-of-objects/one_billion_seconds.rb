3ff1836ce6dbfc114ddab4eb76f1667dd87824df
Dan <djclissold@yahoo.co.uk>
2016-06-17 09:41:11 +0100

    One billion second program complete and working.

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,11 @@
-# your code here
+puts 'Enter your birth year.'
+y = gets.chomp.to_i
+puts 'Enter your birth month.'
+m = gets.chomp.to_i
+puts 'Enter your birthday (day of month only).'
+d = gets.chomp.to_i
+birthday = Time.local(y, m, d)
+onebillionbday = birthday + 1000000000
+puts
+puts "You will be/were one billion seconds old at #{onebillionbday}"
+puts
