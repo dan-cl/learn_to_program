9c811e59c387b856274080ab1af169ca0ffbde5e
Dan <djclissold@yahoo.co.uk>
2016-06-19 10:36:31 +0100

    Birthday helper program complete and working

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,19 @@
-# your code here
+bdayarray = []
+bdayhash = Hash.new
+
+File.readlines('birthdates.txt').each do |line|
+  bdayarray = line.split(',')
+  name = bdayarray[0]
+  date = bdayarray[1] + bdayarray [2]
+  bdayhash[name] = date
+
+end
+
+puts'Enter name'
+namecheck = gets.chomp
+
+if bdayhash.include? (namecheck)
+  puts bdayhash[namecheck]
+else
+  puts 'Name not found.'
+end
