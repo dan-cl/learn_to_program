74047ce5cb1392384c55aa543b4cc0f7ed19aa3f
Dan <djclissold@yahoo.co.uk>
2016-06-20 21:35:42 +0100

    Better program logger program complete and working.

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,22 @@
 def log desc, &block
-  # your code here
+  puts "#{$indent}Start of" + desc + '...'
+  $indent = $indent + '  '
+  result = block.call
+  $indent = $indent.chomp('  ')
+  puts "#{$indent}..." + desc + ' finshed, returning: ' + result.to_s
+end
+
+$indent = ''
+
+log ' the outer (first) block' do
+   log ' the first inner block' do
+     log ' the inner blocks child' do
+     1**1 + 2**2
+   end
+  end
+
+   log ' the second inner block' do
+   puts ' this text is from the second inner block'
+   end
+   5 == 10
 end
